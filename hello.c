ac21e36507d546fcc55bc455dd1fb7a701b1d5cf
coderzaz@gmail.com <coderzaz@gmail.com>
2018-01-31 20:28:13 -0200

    Funcao soma

--- hello.c
+++ hello.c
@@ -1,8 +1,17 @@
 #include <stdio.h>
+int soma()
+{
+  	int a = 2;
+	int b = 3;
+        int soma;
 
+	soma = a + b;
+	
+}
 int main(){
 	printf("Ola Git\n");
         printf("Novo comentario!\n");
-
+     
+        printf("A soma %i\n", soma());
 return (0);
 }
